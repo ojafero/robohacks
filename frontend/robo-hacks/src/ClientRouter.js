002861da9b89176206800eb8a6d62c889a754edf
khalil0525 <collinskhalil@hotmail.com>
2022-10-08 19:33:50 -0400

    added socket context to clientrouter

--- frontend/robo-hacks/src/ClientRouter.js
+++ frontend/robo-hacks/src/ClientRouter.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Landing from "./pages/Landing/Landing";
+import "antd/dist/antd.css";
+import { SocketContext, socket } from "./context/socket";
 const ClientRouter = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/landing" element={<Landing />} />
-    </Routes>
+    <SocketContext.Provider value={socket}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/landing" element={<Landing />} />
+      </Routes>
+    </SocketContext.Provider>
   );
 };
 
