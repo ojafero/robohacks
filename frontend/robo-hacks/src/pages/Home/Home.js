a5300b920f9ccf8f4c34034185eaca0bc043a4d6
khalil0525 <collinskhalil@hotmail.com>
2022-10-08 13:15:53 -0400

    created the page components and their style sheets

---
+++ frontend/robo-hacks/src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import styles from "./Home.module.css";
+import react from "react";
+import {
+  Layout,
+  Header,
+  Content,
+  Footer,
+  Button,
+  DatePicker,
+  Space,
+  version,
+} from "antd";
+const Home = (props) => {
+  return <Layout></Layout>;
+};
+
+export default Home;
---
+++ frontend/robo-hacks/src/pages/Landing/Landing.js
@@ -0,0 +1,14 @@
+import styles from "./Landing.module.css";
+import react from "react";
+import {
+  Layout,
+  Header,
+  Content,
+  Footer,
+  Button,
+  DatePicker,
+  Space,
+  version,
+} from "antd";
+const Landing = () => {};
+export default Landing;
