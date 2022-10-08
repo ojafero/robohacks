541e7d7a4de0f36289e16c74c5ae34a95d45186d
khalil0525 <collinskhalil@hotmail.com>
2022-10-08 15:44:32 -0400

    created routes

--- frontend/robo-hacks/src/ClientRouter.js
+++ frontend/robo-hacks/src/ClientRouter.js
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Landing from "./pages/Landing/Landing";
 const ClientRouter = () => {
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/landing" element={<Landing />} />
-  </Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/landing" element={<Landing />} />
+    </Routes>
+  );
 };
 
 export default ClientRouter;
