a93c686a9fcb3814b89e4d09786b7fc868786a95
Khalil Collins <Collinskhalil@hotmail.com>
2023-09-01 16:41:24 -0400

    updated ytdl

--- backend/controller/youtube-transcriber.js
+++ backend/controller/youtube-transcriber.js
@@ -1,5 +1,5 @@
 const {
-  YtAudioDownloadService,
+	YtAudioDownloadService,
 } = require("../services/AudioDownloaderService");
 const speechSdk = require("microsoft-cognitiveservices-speech-sdk");
 
@@ -9,6 +9,7 @@ const { TextSummary } = require("../services/text-summary");
 const { TextToxicity } = require("../services/text-toxicity");
 const toxicity = require("@tensorflow-models/toxicity");
 const cohere = require("cohere-ai");
+// ud6X30oR9raWUET3dFxRK7tiIyCV7pWqhP5AFtyM
 
 const io = socket.get();
 let client = null;
@@ -17,78 +18,78 @@ const redis = createClient();
 redis.connect();
 
 const loadingStates = [
-  "Video is being downloaded",
-  "Video is being transcribed",
-  "Transcript is being filtered for harmful content",
-  "A summary is being generated",
+	"Video is being downloaded",
+	"Video is being transcribed",
+	"Transcript is being filtered for harmful content",
+	"A summary is being generated",
 ];
 
 redis.on("error", (err) => console.log("Redis Client Error", err));
 
 io.on("connection", (socket) => {
-  client = socket;
-  console.log("client connected");
-  client.on("disconnect", () => {
-    console.log("client disconnected");
-  });
+	client = socket;
+	console.log("client connected");
+	client.on("disconnect", () => {
+		console.log("client disconnected");
+	});
 });
 
 function youtubeTranscriberController(req, res) {
-  const dummyResponse = {
-    summary: "This is a summary",
-    sentences: [
-      { sentence: "sentence1", toxicity: true },
-      { sentence: "sentence2", toxicity: false },
-    ],
-  };
+	const dummyResponse = {
+		summary: "This is a summary",
+		sentences: [
+			{ sentence: "sentence1", toxicity: true },
+			{ sentence: "sentence2", toxicity: false },
+		],
+	};
 
-  res.json(dummyResponse);
+	res.json(dummyResponse);
 }
 
 async function youtubeTestController(req, res) {
-  try {
-    const videoUrl = req.body.url;
-    console.log(videoUrl);
-    const cache = await redis.get(videoUrl);
-    if (cache) {
-      console.log("found in cache");
-      res.json(JSON.parse(cache));
-    } else {
-      const audioDownloader = new YtAudioDownloadService();
-      const fileDestination = await audioDownloader.downloadAudio(videoUrl);
-      if (client) {
-        client.emit("loading", JSON.stringify({ message: loadingStates[1] }));
-      }
-      const youtubeTranscriber = new Transcribe(speechSdk);
-      const transcript = await youtubeTranscriber.convertSpeechIntoText(
-        fileDestination
-      );
-      const transcriptFormatted = transcript.trim().replaceAll("\n", "");
-      if (client) {
-        client.emit("loading", JSON.stringify({ message: loadingStates[2] }));
-      }
+	try {
+		const videoUrl = req.body.url;
+		console.log(videoUrl);
+		const cache = await redis.get(videoUrl);
+		if (cache) {
+			console.log("found in cache");
+			res.json(JSON.parse(cache));
+		} else {
+			const audioDownloader = new YtAudioDownloadService();
+			const fileDestination = await audioDownloader.downloadAudio(videoUrl);
+			if (client) {
+				client.emit("loading", JSON.stringify({ message: loadingStates[1] }));
+			}
+			const youtubeTranscriber = new Transcribe(speechSdk);
+			const transcript = await youtubeTranscriber.convertSpeechIntoText(
+				fileDestination
+			);
+			const transcriptFormatted = transcript.trim().replaceAll("\n", "");
+			if (client) {
+				client.emit("loading", JSON.stringify({ message: loadingStates[2] }));
+			}
 
-      let sentences = transcriptFormatted.split(".");
-      const tensorflow = new TextToxicity(toxicity);
-      const toxicityScores = await tensorflow.detectToxicity(0.5, sentences);
+			let sentences = transcriptFormatted.split(".");
+			const tensorflow = new TextToxicity(toxicity);
+			const toxicityScores = await tensorflow.detectToxicity(0.5, sentences);
 
-      if (client) {
-        client.emit("loading", JSON.stringify({ message: loadingStates[3] }));
-      }
+			if (client) {
+				client.emit("loading", JSON.stringify({ message: loadingStates[3] }));
+			}
 
-      const summaryModel = new TextSummary(cohere);
-      const summary = await summaryModel.generate(transcriptFormatted);
-      const data = {
-        summary: summary.trim().replaceAll("\n", ""),
-        sentences: toxicityScores,
-      };
-      await redis.set(videoUrl, JSON.stringify(data));
-      res.json(data);
-    }
-  } catch (e) {
-    console.log(e);
-    res.status(400).send(e);
-  }
+			const summaryModel = new TextSummary(cohere);
+			const summary = await summaryModel.generate(transcriptFormatted);
+			const data = {
+				summary: summary.trim().replaceAll("\n", ""),
+				sentences: toxicityScores,
+			};
+			await redis.set(videoUrl, JSON.stringify(data));
+			res.json(data);
+		}
+	} catch (e) {
+		console.log(e);
+		res.status(400).send(e);
+	}
 }
 
 exports.youtubeTranscriberController = youtubeTranscriberController;
